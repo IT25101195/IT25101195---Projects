80c1814b09fc09403c9aa9509151b67aeceb07bf
Thanuja Perera <it25101195@my.sliit.lk>
2026-01-31 19:41:07 +0530

    Delete Test3 directory

--- Test3/Test_0/Test1/New/js.js
+++
@@ -1,7 +0,0 @@
-// Smooth scroll to section
-function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-}
-
-// Set current year in footer
-document.getElementById("year").textContent = new Date().getFullYear();
